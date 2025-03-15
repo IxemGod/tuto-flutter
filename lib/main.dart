232b40399d2784c60a9a35fbe6eabd549b8b55c7
axelJaunet <axel@lapsa-lab.fr>
2025-03-15 20:34:49 +0100

    Flutter StateFull Widget

--- lib/main.dart
+++ lib/main.dart
@@ -6,17 +6,33 @@ void main() {
       home: HomePage()
   ));
 }
+class HomePage extends StatefulWidget{
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() {
+    return HomePageState();
+  }
+
+}
+
 
 // Class de la page d'accueil en stateless car elle n'est pas dynamique
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+class HomePageState extends State<HomePage> {
+
+  int counter = 0;
+
   @override
   Widget build(BuildContext context){
     return Scaffold(
       appBar: AppBar(title: const Text("Appli"),elevation: 12,),
-      body: const Center(child: Text("Banjour"),),
+      body: Center(child: Text("Banjour $counter"),),
       floatingActionButton: FloatingActionButton(
-          onPressed: (){},
+          onPressed: (){
+            setState(() {
+              counter += 1;
+            });
+          },
         child: Icon(Icons.add),
       ),
     );
