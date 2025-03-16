8ab24a1c44642f134da85e6c20e2773fffa2843e
axelJaunet <axel@lapsa-lab.fr>
2025-03-16 11:28:21 +0100

    ListView part 2

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,12 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(
+  runApp(MaterialApp(
       home: HomePage()
   ));
 }
 class HomePage extends StatefulWidget{
-  const HomePage({super.key});
+  HomePage({super.key});
 
   @override
   State<HomePage> createState() {
@@ -18,22 +18,23 @@ class HomePage extends StatefulWidget{
 
 // Class de la page d'accueil en stateless car elle n'est pas dynamique
 class HomePageState extends State<HomePage> {
-
+  final List images = [
+    "https://upload.wikimedia.org/wikipedia/commons/1/14/CCRM_Renault_Kerax_-_Gallin_%281%29.jpg",
+    "https://lh5.googleusercontent.com/proxy/8UzeMa7FA8rWxFhJ3rzT850pXLErvspFFulHp5AuiqP9AEZLAfF_CTfpvvNZ0dTtyMa2-0NJUNmrHSVKRgBnKK7GIQPAFOD9ArHPiuZwlPVTipeUqNRx",
+    "https://allo18.fr/wp-content/uploads/2023/12/OUVERTURE.jpg"
+  ];
   @override
   Widget build(BuildContext context){
     return Scaffold(
       appBar: AppBar(title: const Text("Column"),elevation: 12,),
-      body: ListView(
-        scrollDirection: Axis.vertical,
-        children: [
-          // La taille de l'image s'adapte tout seule à la fenêtre
-          Image.network("https://upload.wikimedia.org/wikipedia/commons/1/14/CCRM_Renault_Kerax_-_Gallin_%281%29.jpg", width: 250,),
-          Text("Camion Citerne Rurale"),
-          Image.network("https://lh5.googleusercontent.com/proxy/8UzeMa7FA8rWxFhJ3rzT850pXLErvspFFulHp5AuiqP9AEZLAfF_CTfpvvNZ0dTtyMa2-0NJUNmrHSVKRgBnKK7GIQPAFOD9ArHPiuZwlPVTipeUqNRx", width: 250,),
-          Text("Fourgon Pompe Tone"),
-          Image.network("https://allo18.fr/wp-content/uploads/2023/12/OUVERTURE.jpg", width: 250,),
-          Text("Bras Élévateur Articulé")
-        ],
+      body: ListView.builder(
+        itemCount: images.length,
+        itemBuilder: (context, index){
+          return Container(
+            margin: const EdgeInsets.all(10),
+            child: Image.network(images[ind ex], height: 110,)
+          );
+        },
       ),
     );
   }
