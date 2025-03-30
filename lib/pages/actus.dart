36481249b38b649cc0bec9a78131ef857a22c946
axelJaunet <axel@lapsa-lab.fr>
2025-03-30 21:48:46 +0200

    Vidéo Flutter AppBar

--- lib/pages/actus.dart
+++ lib/pages/actus.dart
@@ -1,11 +1,27 @@
 import 'package:flutter/material.dart';
 
 
-class ActusPage extends StatelessWidget{
+class ActusPage extends StatefulWidget {
   @override
-  Widget build(BuildContext context){
-    return Center(
-      child: Text("Actu"),
+  _ActuPageState createState() => _ActuPageState();
+}
+
+class _ActuPageState extends State<ActusPage>{
+  @override
+    Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        leading: Icon(Icons.menu), // Partie tout a gauche
+        title: Text("Mon application"),
+        elevation: 18.0,
+        actions: [
+          IconButton(onPressed: (){}, icon: Icon(Icons.search)),
+          IconButton(onPressed: (){}, icon: Icon(Icons.more_vert))
+        ],
+      ),
+      body: Center(
+        child: Text("Flutter"),
+      ),
     );
   }
 }
